e67f069aa84f63b7f97c22952198bb72cfa10612
Milagros O. Almidon <orihuela.ms@gmail.com>
2022-05-04 05:49:13 -0500

    feat: add subscription success message

--- apps/website/src/app/elements/atoms/Button/index.tsx
+++ apps/website/src/app/elements/atoms/Button/index.tsx
@@ -2,10 +2,13 @@ import { ReactNode } from "react";
 
 type Props = {
   children: ReactNode,
+  onClick: () => void;
 }
 
-const Button = ({ children }:Props) => (
-  <div className="py-2 px-4 h-8 bg-secondary-blue-500 rounded text-center text-button-05">
+const Button = ({ children, onClick }:Props) => (
+  <div 
+    className="py-2 px-4 lg:py-4 lg:px-6 h-8 lg:h-12 flex items-center bg-secondary-blue-500 rounded text-center text-button-05 lg:text-button-04 font-bold hover:bg-secondary-blue-400 hover:cursor-pointer" 
+    onClick={() => onClick()}>
     {children}
   </div>
 );
