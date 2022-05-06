bfc6a7be070bd4ffc36195d69b5f232d6a8e6293
Milagros O. Almidon <orihuela.ms@gmail.com>
2022-05-06 05:56:14 -0500

    fix: add max width for gridwrapper

--- apps/website/src/app/elements/organisms/GridWrapper/index.tsx
+++ apps/website/src/app/elements/organisms/GridWrapper/index.tsx
@@ -9,7 +9,7 @@ const GridWrapper: FC<Props> = ({
   children,
 }) => (
   <div className={className}>
-    <div className="mx-auto w-[calc(100%-3rem)] md:w-[calc(100%-5rem)] lg:w-[calc(100%-7.5rem)] xl:w-[calc(100%-15rem)]">
+    <div className="mx-auto w-[calc(100%-3rem)] md:w-[calc(100%-5rem)] lg:w-[calc(100%-7.5rem)] xl:w-[calc(100%-15rem)] max-w-[calc(1920px-15rem)]">
       <div className="roots-grid">
         {children}
       </div>
