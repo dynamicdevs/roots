88e10582d2d71e0b77f9575c7aa032fc2145443c
Milagros O. Almidon <orihuela.ms@gmail.com>
2022-05-06 11:54:37 -0500

    fix: height card link element

--- apps/website/src/app/elements/molecules/LinkCard/index.tsx
+++ apps/website/src/app/elements/molecules/LinkCard/index.tsx
@@ -16,7 +16,7 @@ const LinkCard:FC<Props> = ({
   iconType = 'solid'
 }) => (
   <a href={url} target="_blank" rel="noreferrer">
-    <div className="bg-secondary-blue-500 text-primary-dynamic-500 rounded py-4 px-14 relative hover:bg-secondary-blue-400 transition-colors">
+    <div className="bg-secondary-blue-500 text-primary-dynamic-500 rounded py-5 lg:py-4 px-14 relative hover:bg-secondary-blue-400 transition-colors">
       <SymbolIcon name={iconName} iconClass="symbol-md" type={iconType} className="absolute top-1/2 -translate-y-1/2 left-4" />
       <p className="text-center text-paragraph-04 md:text-paragraph-03 font-bold text-primary-grey-600 w-full line-clamp-2">{text}</p>
     </div>
