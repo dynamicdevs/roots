0c0359cd850b265e3033d8034443640f262f5ca5
Milagros O. Almidon <orihuela.ms@gmail.com>
2022-05-06 12:17:41 -0500

    fix: improve event code

--- apps/website/src/app/elements/atoms/Button/index.tsx
+++ apps/website/src/app/elements/atoms/Button/index.tsx
@@ -10,7 +10,7 @@ const Button = ({ children, onClick }:Props) => (
     className="
         py-2 px-4 lg:py-4 lg:px-6 h-8 lg:h-12 flex items-center rounded text-center text-button-05 lg:text-button-04 font-bold
       bg-secondary-blue-500 hover:bg-secondary-blue-400 hover:cursor-pointer transition-colors" 
-    onClick={() => onClick()}>
+    onClick={onClick}>
     {children}
   </div>
 );
