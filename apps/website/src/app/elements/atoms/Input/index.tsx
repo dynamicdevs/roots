2ba7c202561ff8c3711f768e316e7253768f1da7
Milagros O. Almidon <orihuela.ms@gmail.com>
2022-05-06 11:41:28 -0500

    fix: improve hover input behaviour

--- apps/website/src/app/elements/atoms/Input/index.tsx
+++ apps/website/src/app/elements/atoms/Input/index.tsx
@@ -11,8 +11,8 @@ const Input = ({ placeholder, onKeyDown }:Props) => {
  
   return (
     <div className="
-        px-2 lg:px-4 bg-primary-grey-600 h-8 lg:h-12 rounded w-full 
-      focus-within:border-secondary-blue-500 focus-within:border-2 hover:border-secondary-blue-500 hover:border-2 transition-colors">
+        px-2 lg:px-4 bg-primary-grey-600 h-8 lg:h-12 rounded w-full border-2 border-transparent
+      focus-within:border-secondary-blue-500 hover:border-secondary-blue-500 transition-colors">
       <input 
         value={text} 
         type="text" 
