23b49f96d4167a50507253121076df92ff21aecd
Milagros O. Almidon <orihuela.ms@gmail.com>
2022-05-12 11:44:57 -0500

    feat: add image prop for layout element

--- apps/website/src/app/elements/organisms/Layout/index.tsx
+++ apps/website/src/app/elements/organisms/Layout/index.tsx
@@ -1,14 +1,16 @@
 import { ReactNode } from "react";
 import Footer from "@app/elements/organisms/Footer";
 import Header from "@app/elements/organisms/Header";
+import { ImageProps } from "@app/types/image.props";
 
 type Props = {
+  image: ImageProps;
   children: ReactNode;
 }
 
-const Layout = ({ children }:Props) => (
+const Layout = ({ image, children }:Props) => (
   <>
-    <Header image={{ url: './assets/dd-image-profile.png', alt: 'Imagen de Perfil'}} />
+    <Header image={{ url: image.url, alt: image.alt }} />
     <div className="min-h-[calc(100vh-3rem)]">
       {children}
     </div>
