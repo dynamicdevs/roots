03b3abd2156b728cfb630a400be2536803a22387
Milagros O. Almidon <orihuela.ms@gmail.com>
2022-05-12 11:45:38 -0500

    feat: conditional render

--- apps/website/src/app/app.tsx
+++ apps/website/src/app/app.tsx
@@ -1,37 +1,43 @@
 import { useEffect, useState } from 'react';
 import { client } from '@services/contentfulClient';
-import { RootsType } from '@services/types';
 import { formatData } from '@utils/util';
 import Layout from '@app/elements/organisms/Layout';
 import { Home } from '@app/page/Home';
+import { RootsProps } from './types/roots.props';
 
 export function App() {
+  const [item, setItem] = useState<RootsProps>();
 
-  const [items, setItems] = useState<RootsType[]>([]);
-  
   useEffect(() => {
     fetchData();
   }, []);
 
-  const fetchData = async() => {
+  const fetchData = async () => {
     try {
-
-      const response = await client.getEntries({ content_type: 'lt-home', include: 2 });
+      const response = await client.getEntries({
+        content_type: 'lt-home',
+        include: 2,
+      });
       const formattedData = formatData(response);
 
-      setItems(formattedData);
+      setItem(formattedData[0]);
       
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
-  }
+  };
 
-  return (    
-    <Layout>      
-      <Home data={ items[0] }/>
-    </Layout>
+  return (
+    <div>
+      {item ? (
+        <Layout image={item.image}>
+          <Home data={item} />
+        </Layout>
+      ) : (
+        <div>Cargando</div>
+      )}
+    </div>
   );
 }
 
 export default App;
-
